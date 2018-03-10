bebf6b4925c7ba81f2f01d3022a0e83fa9046a75
Xavi Ivars <xavi.ivars@gmail.com>
2018-03-11 00:28:25 +0100

    Changing footer

---
+++ footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Footer
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ * @package momoyo
+ */
+?>
+
+		</div><!-- #content -->
+	</div><!-- #page -->
+</div><!-- container -->
+
+	<footer id="colophon" class="site-footer">
+	<div class="container">
+
+         <div id="credit" class="site-info">
+             <p>
+                 Disseny i programació:<a href="http://xavi.ivars.me">
+                 <img src="/wp-content/themes/bandadebenissa/imatges/logo-xavi.png">Xavi Ivars</a>
+             </p>
+         </div><!-- .site-info -->
+
+		</div>
+	</footer><!-- #colophon -->
+
+
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 add_action( 'wp_enqueue_scripts', 'borrellons_estils' );
 function borrellons_estils() {
 	    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
+	    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
 }
 ?>
